3c8ee6ebc3761a9852c786a6c142c1383f12eee6
dbotia <dbotia@bancolombia.com.co>
2024-09-17 14:44:01 -0500

    Ejercicio1 Terminado

---
+++ Ejercicio1/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Utilizamos un bucle for para imprimir los números del 10 al 1
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+
+        }
+    }
+}
